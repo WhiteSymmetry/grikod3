d509902393cbb6bd464370f35f9117318885b658
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-07-06 13:49:48 +0300

    Enhance setup.py for platform-specific extension
    Added platform and sys imports for dynamic extension naming.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # setup.py
 
+import sys
+import platform
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
@@ -10,4 +12,10 @@ extensions = [
 
 setup(
     ext_modules=cythonize(extensions, compiler_directives={'language_level': 3}),
+)
+
+
+ext = Extension(
+    name=f"grikod3.grikod3_{platform.system()}_py{sys.version_info.major}{sys.version_info.minor}",
+    sources=["grikod3/grikod3.py"]
 )
